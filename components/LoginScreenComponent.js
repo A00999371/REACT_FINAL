4301e144b16281f640237649a303e0a91e798779
a-mori <almoriex@gmail.com>
2017-11-28 22:24:53 -0800

    Finished login

--- components/LoginScreenComponent.js
+++ components/LoginScreenComponent.js
@@ -1,40 +1,110 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Text, View, Button, TextInput } from 'react-native';
+import PropTypes, { func } from 'prop-types';
+import { Text, View, Button, TextInput, AsyncStorage, Alert, StyleSheet } from 'react-native';
+import { NavigationAction, NavigationActions } from 'react-navigation';
 
-export default class HomeScreenComponent extends Component {
+export default class LoginScreenComponent extends Component {
 
     constructor() {
         super();
         this.state = {
             text: ''
         }
+        this.checkProfile = this.checkProfile.bind(this);
+        this.createProfile = this.createProfile.bind(this);
     }
 
     static navigationOptions = {
         title: 'Mind Games',
     };
+
+    //Create an AsyncStorage profile and assign a default value for the highscore and avatar
+    async createProfile(text) {
+        try {
+            //Create an AsyncStorage item with a default value and key of what the user entered
+            await AsyncStorage.setItem(text, "0 ../img/profile.png");
+            console.log("Created profile: " + text);
+        } catch (error) {
+            //Error saving data
+            console.log(error.message);
+        }
+    }
+
+    //See if the AsyncStorage profile exists, and if it does, use that as the login profile
+    async checkProfile(text) {
+        try {
+            //Attempt to get the AsyncStorage item that matches the text the user entered
+            const profile = await AsyncStorage.getItem(text);
+
+            if (profile !== null) {
+                //Set AsyncStorage items highscore, profile, and avatar to equal what the user is trying to login with and send the user to main menu
+                var words = profile.split(" ");
+                await AsyncStorage.setItem("Highscore", words[0]);
+                await AsyncStorage.setItem("Avatar", words[1]);
+                await AsyncStorage.setItem("Profile", text);
+
+                const navigateAction = NavigationActions.navigate({
+                    routeName: 'Menu',
+                    params: {}
+                });
+
+                this.props.navigation.dispatch(navigateAction);
+            } else {
+                //Alert popup saying user does not exist
+                Alert.alert("Invalid", "Profile does not exist.");
+            }
+        } catch (error) {
+            //Error getting data
+            console.log(error.message);
+        }
+    }
+
     render() {
-        const {navigate} = this.props.navigation;
         return (
-            <View>
-                <Text>By: MARKSMEN Games</Text>
+            <View style={{flex: 1, alignContent: 'center', justifyContent: 'flex-start'}}>
                 <Button
                     //TODO Place in an if statement for if the entered data matches login data from file then goes to Main Menu
-                    onPress={() => navigate('Menu')}
+                    onPress={() => this.checkProfile(this.state.text)}
                     title="Login"
+                    style={styles.but}
                 />
+
                 <Button
+                    onPress={() => this.createProfile(this.state.text)}
                     title="Create Profile"
-                    //TODO add onPress function to create profile using textinput and save to file
+                    style={styles.but}
                 />
+
                 <TextInput
-                    style={{height: 40, borderColor: 'gray', width: 300}}
+                    style={styles.inp}
                     onChangeText={(text) => this.setState({text})}
                     value={this.state.text} 
                     placeholder="Enter Profile Name"
                 />
+
+                <Text style={styles.te}>By: MARKSMEN Games</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    but: {
+        height: 30,
+        alignSelf: 'stretch'
+    },
+    te: {
+        fontSize: 10,
+        position: 'absolute',
+        bottom: 0,
+        textAlign: 'center',
+        alignSelf: 'center'
+    },
+    inp: {
+        marginTop: 150,
+        alignSelf: 'center',
+        width: 250,
+        height: 30,
+        textAlign: 'center'
+    }
+});
